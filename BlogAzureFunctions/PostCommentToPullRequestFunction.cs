c304ebd5e34f1867c148767dd87615ac580a5155
Damien Guard <damieng@gmail.com>
2018-05-27 13:25:44 -0700

    Add comments, remove non-comment functions.

--- BlogAzureFunctions/PostCommentToPullRequestFunction.cs
+++ BlogAzureFunctions/PostCommentToPullRequestFunction.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Octokit;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Configuration;
 using System.Linq;
 using System.Net;
@@ -15,9 +17,9 @@ namespace BlogAzureFunctions
 {
     public static class PostCommentToPullRequestFunction
     {
-        struct MissingRequiredValue { }
-        static readonly Regex pathValidChars = new Regex(@"[^a-zA-Z-]");
-        static readonly Regex emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        struct MissingRequiredValue { } // Placeholder for missing required form values
+        static readonly Regex validPathChars = new Regex(@"[^a-zA-Z-]"); // Valid characters when mapping from the blog post slug to a file path
+        static readonly Regex validEmail = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$"); // Simplest form of email validation
 
         [FunctionName("PostComment")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestMessage request)
@@ -27,57 +29,85 @@ namespace BlogAzureFunctions
                 await CreateCommentAsPullRequest(comment);
 
             if (errors.Any())
-            {
                 return request.CreateErrorResponse(HttpStatusCode.BadRequest, String.Join("\n", errors));
-            }
+
+            if (!Uri.TryCreate(form["redirect"], UriKind.Absolute, out var redirectUri))
+                return request.CreateResponse(HttpStatusCode.OK);
 
             var response = request.CreateResponse(HttpStatusCode.Redirect);
-            if (form["redirect"] != null)
-                response.Headers.Location = new Uri(form["redirect"]);
+            response.Headers.Location = redirectUri;
             return response;
         }
 
         private static async Task<PullRequest> CreateCommentAsPullRequest(Comment comment)
         {
+            // Create the Octokit client
             var github = new GitHubClient(new ProductHeaderValue("PostCommentToPullRequest"),
                 new Octokit.Internal.InMemoryCredentialStore(new Credentials(ConfigurationManager.AppSettings["GitHubToken"])));
 
+            // Get a reference to our GitHub repository
             var repoOwnerName = ConfigurationManager.AppSettings["PullRequestRepository"].Split('/');
             var repo = await github.Repository.Get(repoOwnerName[0], repoOwnerName[1]);
 
+            // Create a new branch from the default branch
             var defaultBranch = await github.Repository.Branch.Get(repo.Id, repo.DefaultBranch);
             var newBranch = await github.Git.Reference.Create(repo.Id, new NewReference($"refs/heads/comment-{comment.id}", defaultBranch.Commit.Sha));
-            var fileRequest = new CreateFileRequest($"Comment by {comment.name} on {comment.post_id}", new SerializerBuilder().Build().Serialize(comment), newBranch.Ref);
-            fileRequest.Committer = new Committer(comment.name, comment.email, comment.date);
+
+            // Create a new file with the comments in it
+            var fileRequest = new CreateFileRequest($"Comment by {comment.name} on {comment.post_id}", new SerializerBuilder().Build().Serialize(comment), newBranch.Ref)
+            {
+                Committer = new Committer(comment.name, comment.email, comment.date)
+            };
             await github.Repository.Content.CreateFile(repo.Id, $"_data/comments/{comment.post_id}/{comment.id}.yml", fileRequest);
 
-            return await github.Repository.PullRequest.Create(repo.Id, new NewPullRequest(fileRequest.Message, newBranch.Ref, defaultBranch.Name) { Body = comment.message });
+            // Create a pull request for the new branch and file
+            return await github.Repository.PullRequest.Create(repo.Id, new NewPullRequest(fileRequest.Message, newBranch.Ref, defaultBranch.Name)
+            {
+                Body = comment.message
+            });
         }
 
         private static object ConvertParameter(string parameter, Type targetType)
         {
-            return String.IsNullOrWhiteSpace(parameter) ? null : System.ComponentModel.TypeDescriptor.GetConverter(targetType).ConvertFrom(parameter);
+            return String.IsNullOrWhiteSpace(parameter)
+                ? null
+                : TypeDescriptor.GetConverter(targetType).ConvertFrom(parameter);
         }
 
-        private static bool TryCreateCommentFromForm(System.Collections.Specialized.NameValueCollection form, out Comment comment, out List<string> errors)
+        /// <summary>
+        /// Try to create a Comment from the form.  Each Comment constructor argument will be name-matched
+        /// against values in the form. Each non-optional arguments (those that don't have a default value)
+        /// not supplied will cause an error in the list of errors and prevent the Comment from being created.
+        /// </summary>
+        /// <param name="form">Incoming form submission as a <see cref="NameValueCollection"/>.</param>
+        /// <param name="comment">Created <see cref="Comment"/> if no errors occurred.</param>
+        /// <param name="errors">A list containing any potential validation errors.</param>
+        /// <returns>True if the Comment was able to be created, false if validation errors occurred.</returns>
+        private static bool TryCreateCommentFromForm(NameValueCollection form, out Comment comment, out List<string> errors)
         {
             var constructor = typeof(Comment).GetConstructors()[0];
             var values = constructor.GetParameters()
-                .ToDictionary(p => p.Name, p => ConvertParameter(form[p.Name], p.ParameterType) ?? (p.HasDefaultValue ? p.DefaultValue : new MissingRequiredValue()));
+                .ToDictionary(
+                    p => p.Name,
+                    p => ConvertParameter(form[p.Name], p.ParameterType) ?? (p.HasDefaultValue ? p.DefaultValue : new MissingRequiredValue())
+                );
 
             errors = values.Where(p => p.Value is MissingRequiredValue).Select(p => $"Form value missing for {p.Key}").ToList();
-            if (values["email"] is string s && !emailRegex.IsMatch(s))
+            if (values["email"] is string s && !validEmail.IsMatch(s))
                 errors.Add("email not in correct format");
 
             comment = errors.Any() ? null : (Comment)constructor.Invoke(values.Values.ToArray());
             return !errors.Any();
         }
 
+        /// <summary>
+        /// Represents a Comment to be written to the repository in YML format.
+        /// </summary>
         private class Comment
         {
             public Comment(string post_id, string message, string name, string email, DateTime? date = null, Uri url = null, int? id = null, string gravatar = null)
             {
-                this.post_id = pathValidChars.Replace(post_id, "-");
+                this.post_id = validPathChars.Replace(post_id, "-");
                 this.message = message;
                 this.name = name;
                 this.email = email;
